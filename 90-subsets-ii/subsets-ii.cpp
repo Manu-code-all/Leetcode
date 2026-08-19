f841c957acc17e9479a2972777f19484beeb0c5c
Manu-code-all <manug9868@gmail.com>
2026-08-19 15:21:07 +0530

    Time: 3 ms (31.25%) | Memory: 11.7 MB (15.05%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,21 +1,29 @@
+void fun(vector<int>&nums,vector<int>&tmp,vector<vector<int>>&ans,map<vector<int>,int>&m1){
+    sort(tmp.begin(),tmp.end());
+    if(m1.find(tmp)!=m1.end()) 
+        return ;
+    ans.push_back(tmp);
+    m1[tmp]++;
+    if(nums.size()==0){
+        return ;
+    }
+    for(int i=0;i<nums.size();i++){
+        vector<int>ip(nums),op(tmp);
+        // ip={1,2,4} op={}
+        op.push_back(nums[i]);
+        for(int j=0;j<=i;j++)
+        ip.erase(ip.begin());
+        fun(ip,op,ans,m1);
+    }
+}
 class Solution {
 public:
-    vector<vector<int>> subsetsWithDup(vector<int>nums){
-        vector<int> temp;
-        sort(nums.begin(),nums.end());
-        solve(nums,0,temp);
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<int>tmp;
+        vector<vector<int>>ans;
+        map<vector<int>,int>m1;
+        fun(nums,tmp,ans,m1);
+        sort(ans.begin(),ans.end());
         return ans;
     }
-    vector<vector<int>> ans;
-    void solve (vector<int>& nums,int start, vector<int>& temp){
-        ans.push_back(temp);
-        for(int i = start; i < nums.size(); i++) {
-            if(i > start && nums[i]==nums[i-1])
-                continue;
-            temp.push_back(nums[i]);
-            solve(nums, i + 1, temp);
-            temp.pop_back();
-        }
-    }
-    
 };
