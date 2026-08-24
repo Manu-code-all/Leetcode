c2c8e762dc3d949c2f849372d80dc57345468c94
Manu-code-all <manug9868@gmail.com>
2026-08-24 18:59:15 +0530

    Time: 164 ms (5.06%) | Memory: 91.7 MB (5.36%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -7,24 +7,16 @@ void fun(int s,vector<int>&nums,vector<int>&tmp,vector<vector<int>>&ans,int targ
         }
         return ;
     }
-
     for(int i=0;i<nums.size();i++){
         vector<int>ip=nums;
-
         if(i>0 && nums[i]==nums[i-1]) continue;
-
         //op.push_back(nums[i]);
         for(int j=0;j<=i;j++) ip.erase(ip.begin());
-
         tmp.push_back(nums[i]);
-
         fun(s+nums[i],ip,tmp,ans,target,m1);
-
         tmp.pop_back();
     }
 }
-
-
 class Solution {
 public:
     vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
