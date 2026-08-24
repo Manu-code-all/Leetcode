bf9e9eddb18bc5ef9262f085aff694c3be9e2506
Manu-code-all <manug9868@gmail.com>
2026-08-24 19:03:58 +0530

    Time: 3 ms (25.91%) | Memory: 12.6 MB (6.16%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,22 @@
+void fun(vector<int>&nums,vector<int>&tmp,vector<vector<int>>&ans){
+    if(nums.size()==0){
+    ans.push_back(tmp);
+        return;
+    }
+    for(int i=0;i<nums.size();i++){
+        vector<int>ip(nums),op(tmp);
+        op.push_back(nums[i]);  
+        ip.erase(ip.begin()+i);
+        fun(ip,op,ans);
+    }
+}
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int>tmp;
+        vector<vector<int>>ans;
+        fun(nums,tmp,ans);
+       
+        return ans;
+    }  
+};
