d81da9776db831ac6f5d3a785e6877f8f3a47390
Manu-code-all <manug9868@gmail.com>
2026-09-07 09:16:55 +0530

    Time: 14 ms (22.28%) | Memory: 7.6 MB (88.47%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,12 +1,13 @@
-int fun(int n){
-    if (n<=0)return 0;
-    if (n==1)return 1;
-    return fun(n-1)+fun(n-2);
-}
+
     
 class Solution {
 public:
     int fib(int n) {
         return fun(n);
     }
+    int fun(int n){
+    if (n<=0)return 0;
+    if (n==1)return 1;
+    return fun(n-1)+fun(n-2);
+}
 };
