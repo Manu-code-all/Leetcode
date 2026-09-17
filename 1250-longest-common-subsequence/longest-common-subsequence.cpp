74bb34f72dff5e433a76bba6da7a8a69a0d17ca6
Manu-code-all <manug9868@gmail.com>
2026-09-17 09:16:21 +0530

    Time: 27 ms (58.75%) | Memory: 12.7 MB (89.89%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,24 +1,21 @@
 int dp[1001][1001];
-
-int fun(int i,int j,string &s,string &t){
-    if(i>=s.size() || j>=t.size()) return 0;
+int fun(int i,int j ,string &s,string &t){
+    if(i>=s.size() || j>=t.size())return 0;
 
     if(dp[i][j]!=-1) return dp[i][j];
-
-    int m=0;
-
+    int ans=0;
     if(s[i]==t[j]){
-        int a=1+fun(i+1,j+1,s,t);
-        m=max(m,a);
-    }else{
-        int a=fun(i+1,j,s,t);
-        int b=fun(i,j+1,s,t);
-
-        m=max(m,a);
-        m=max(m,b);
+        int c1=1+fun(i+1,j+1,s,t);
+        ans=max(ans,c1);
     }
+    else{
+        int c1=fun(i+1,j,s,t);
+        int c2=fun(i,j+1,s,t);
+        ans= max(ans,c2);
+        ans=max(ans,c1);
+    }
+    return dp[i][j]=ans ;
 
-    return dp[i][j]=m;
 }
 
 class Solution {
